71e2d752ce6d4f34a317a4483d0dbd3d60ce0db3
Luiz Eduardo <luiztsilva@protonmail.com>
2026-03-27 12:12:06 -0300

    Remove wayfinder dependency

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,13 @@
-import { wayfinder } from '@laravel/vite-plugin-wayfinder';
 import tailwindcss from '@tailwindcss/vite';
 import vue from '@vitejs/plugin-vue';
 import laravel from 'laravel-vite-plugin';
-import { defineConfig } from 'vite';
 import path from 'path';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/js/app.ts'], // Se o arquivo for .ts
+            input: ['resources/js/app.ts'],
             ssr: 'resources/js/ssr.ts',
             refresh: true,
         }),
@@ -21,11 +20,6 @@ export default defineConfig({
                 },
             },
         }),
-        wayfinder({
-            formVariants: true,
-            // Desativado para não travar o build no servidor
-            generateOnRun: false, 
-        }),
     ],
     resolve: {
         alias: {
@@ -34,13 +28,11 @@ export default defineConfig({
         extensions: ['.js', '.ts', '.vue', '.json'],
     },
     build: {
-        // Ativa a minificação pesada para o servidor
-        minify: 'esbuild', 
-        sourcemap: false, // Deixa o build mais leve no servidor
+        minify: 'esbuild',
+        sourcemap: false,
         chunkSizeWarningLimit: 1600,
         rollupOptions: {
             output: {
-                // Organiza melhor os arquivos gerados
                 manualChunks(id) {
                     if (id.includes('node_modules')) {
                         return 'vendor';
